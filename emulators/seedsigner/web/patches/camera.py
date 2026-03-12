812b17a47263daaf4263550e379e141cdc2bd49a
Ren <rensovereign@proton.me>
2026-03-11 22:08:55 -0500

    fix: complete Camera stub with all methods (start_single_frame_mode, etc)
    Camera now has: start/stop, start/stop_video_stream_mode,
    start/stop_single_frame_mode, capture_frame, read_video_stream,
    camera_rotation, CameraConnectionError, WebVideoStream, Singleton base.
    
    Scan will still show 'float division by zero' (time-based, harmless)
    but won't crash on missing camera methods.

--- emulators/seedsigner/web/patches/camera.py
+++ emulators/seedsigner/web/patches/camera.py
@@ -1,5 +1,6 @@
 """
 Stub camera for Pyodide SeedSigner emulator.
+All camera methods return empty/black frames.
 """
 import numpy as np
 from seedsigner.models.singleton import Singleton
@@ -10,8 +11,9 @@ class CameraConnectionError(Exception):
 
 
 class WebVideoStream:
-    def __init__(self):
-        self.frame = np.zeros((480, 640, 3), dtype=np.uint8)
+    def __init__(self, resolution=(512, 384), framerate=12, format="bgr"):
+        self.resolution = resolution
+        self.frame = np.zeros((resolution[1], resolution[0], 3), dtype=np.uint8)
         self.stopped = False
 
     def start(self):
@@ -23,6 +25,17 @@ class WebVideoStream:
     def read(self):
         return self.frame
 
+    def hasCamera(self):
+        return True
+
+
+def set_camera_frame(frame_data):
+    pass
+
+
+def is_camera_active():
+    return False
+
 
 class Camera(Singleton):
     _instance = None
@@ -31,6 +44,8 @@ class Camera(Singleton):
         self.is_active = False
         self._video_stream = None
         self._camera_rotation = 0
+        self._current_frame = None
+        self.framerate = 6
 
     @classmethod
     def get_instance(cls):
@@ -41,28 +56,43 @@ class Camera(Singleton):
 
     def start(self):
         self.is_active = True
-        self._video_stream = WebVideoStream().start()
 
     def stop(self):
         self.is_active = False
         if self._video_stream:
             self._video_stream.stop()
+            self._video_stream = None
 
-    def start_video_stream_mode(self, resolution=(512, 512), framerate=12, format="rgb"):
-        self.start()
-
-    def stop_video_stream_mode(self):
-        self.stop()
-
-    def capture_frame(self):
-        return np.zeros((480, 640, 3), dtype=np.uint8)
+    def start_video_stream_mode(self, resolution=(512, 384), framerate=12, format="bgr"):
+        self._video_stream = WebVideoStream(resolution=resolution, framerate=framerate, format=format)
+        self._video_stream.start()
+        self.is_active = True
 
     def read_video_stream(self, as_image=False):
-        frame = np.zeros((512, 512, 3), dtype=np.uint8)
+        if self._video_stream:
+            frame = self._video_stream.read()
+        else:
+            frame = np.zeros((384, 512, 3), dtype=np.uint8)
         if as_image:
             from PIL import Image
             return Image.fromarray(frame)
         return frame
+
+    def stop_video_stream_mode(self):
+        self.stop()
+
+    def start_single_frame_mode(self, resolution=(720, 480)):
+        self.is_active = True
+        self._current_frame = np.zeros((resolution[1], resolution[0], 3), dtype=np.uint8)
+
+    def capture_frame(self):
+        if self._current_frame is not None:
+            return self._current_frame
+        return np.zeros((480, 720, 3), dtype=np.uint8)
+
+    def stop_single_frame_mode(self):
+        self.is_active = False
+        self._current_frame = None
 
     @property
     def camera_rotation(self):
